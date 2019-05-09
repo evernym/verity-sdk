180b49807167f211decee65f52dc6f137ca6513d
Joe Genereux <josephgenereuxmail@gmail.com>
2019-05-09 17:39:41 -0600

    adds keygen

--- tools/libindylite/src/lib.rs
+++ tools/libindylite/src/lib.rs
@@ -1,7 +1,10 @@
 extern crate sodiumoxide;
 
-pub extern fn indylite_generate_keypair() {
+use sodiumoxide::crypto::sign::ed25519;
 
+pub extern fn indylite_generate_keypair() -> (sodiumoxide::crypto::sign::ed25519::PublicKey, sodiumoxide::crypto::sign::ed25519::SecretKey) {
+    sodiumoxide::init();
+    sodiumoxide::crypto::sign::gen_keypair()
 }
 
 pub extern fn indylite_pack_message() {
@@ -14,8 +17,16 @@ pub extern fn indylite_unpack_message() {
 
 #[cfg(test)]
 mod tests {
+    use crate::indylite_generate_keypair;
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+
+    fn test_generate_key_pair() {
+        println!(indylite_generate_keypair())
     }
 }
