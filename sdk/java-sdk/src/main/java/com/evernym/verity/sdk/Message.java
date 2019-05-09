579ed6fdb3ceff11e8d2e035548ca4db9ddc9e85
Spencer Holman <spencer.holman@evernym.com>
2019-05-09 19:11:59 +0000

    intermediate commit of initial java-sdk

---
+++ sdk/java-sdk/src/main/java/com/evernym/verity/sdk/Message.java
@@ -0,0 +1,13 @@
+package com.evernym.verity.sdk;
+
+public class Message {
+    String encryptedMessage;
+
+    public Message() {
+        
+    }
+
+    public Message(String encryptedMessage) {
+        this.encryptedMessage = encryptedMessage;
+    }
+}
---
+++ sdk/java-sdk/src/main/java/com/evernym/verity/sdk/VeritySdk.java
@@ -0,0 +1,59 @@
+package com.evernym.verity.sdk;
+
+import java.io.IOError;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.hyperledger.indy.sdk.IndyException;
+import org.hyperledger.indy.sdk.wallet.*;
+import org.json.JSONObject;
+
+public class VeritySdk {
+    public VeritySdk() {
+
+    }
+
+    public String handleInboundMessage(String message) {
+        System.out.println("Got message: " + message);
+        return message;
+    }
+
+    public void newConnection() throws IOException {
+        JSONObject json = new JSONObject().put("key", "value");
+        send(json);
+    }
+
+    public void provision() throws IndyException, Exception {
+        String walletConfig = new JSONObject().put("id", "java_wallet").toString();
+        String walletCredentials = new JSONObject().put("key", "12345").toString();
+        Wallet.createWallet(walletConfig, walletCredentials).get();
+        Wallet myWallet = Wallet.openWallet(walletConfig, walletCredentials).get();
+        myWallet.closeWallet().get();
+        Wallet.deleteWallet(walletConfig, walletCredentials).get();
+    }
+
+    private void send(JSONObject json) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        HttpResponse response;
+        try {
+            HttpPost request = new HttpPost("http://localhost:4000");
+            StringEntity params = new StringEntity(json.toString());
+            request.addHeader("content-type", "application/json");
+            request.setEntity(params);
+            response = httpClient.execute(request);
+            // handle response here...
+            if (response != null) {
+                InputStream in = response.getEntity().getContent(); //Get the data in the entity
+            }
+        } catch (Exception ex) {
+            // handle exception here
+        } finally {
+            httpClient.close(); // TODO: How to handle this IOException
+        }
+    }
+}
