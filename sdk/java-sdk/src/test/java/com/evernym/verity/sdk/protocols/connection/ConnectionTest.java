b5364020957563c411edfb47d7aacefb3692fe20
Joe Genereux <josephgenereuxmail@gmail.com>
2019-05-10 12:00:41 -0600

    adds test suite for new proj structure

---
+++ sdk/java-sdk/src/test/java/com/evernym/verity/sdk/protocols/connection/ConnectionTest.java
@@ -0,0 +1,13 @@
+package com.evernym.verity.sdk.protocols.connection;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ConnectionTest {
+
+    @Test
+    public void works() throws Exception {
+        assertTrue(true);
+    }
+}
---
+++ sdk/java-sdk/src/test/java/com/evernym/verity/sdk/utils/MessagePackagingTest.java
@@ -0,0 +1,13 @@
+package com.evernym.verity.sdk.utils;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MessagePackagingTest {
+
+    @Test
+    public void works() throws Exception {
+        assertTrue(true);
+    }
+}
