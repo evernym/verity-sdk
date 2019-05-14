fc365155f852f584737196dc24ed2162474920be
Spencer Holman <spencer.holman@evernym.com>
2019-05-14 23:28:04 +0000

    only use requests library in provision_sdk.py

--- tools/provisioner/provision_sdk.py
+++ tools/provisioner/provision_sdk.py
@@ -5,10 +5,8 @@ import argparse
 import asyncio
 import time
 import os
-import urllib.request
 import sys
 import requests
-from ctypes import *
 
 from indy import did, wallet, crypto
 from indy.error import ErrorCode, IndyError
@@ -18,42 +16,19 @@ def parse_args():
     parser.add_argument("AGENCY_URL")
     parser.add_argument("WALLET_KEY")
     parser.add_argument("--wallet-name", help="optional name for libindy wallet")
-    parser.add_argument("--wallet-type", help="optional type of libindy wallet")
-    parser.add_argument("--agent-seed", help="optional seed used to create enterprise->agent DID/VK")
-    parser.add_argument("--enterprise-seed", help="optional seed used to create enterprise DID/VK")
-    parser.add_argument("--verbose", action="store_true")
+    # parser.add_argument("--wallet-type", help="optional type of libindy wallet")
+    parser.add_argument("--verbose", action="store_true") # FIXME: Doesn't support new indy logger
     return parser.parse_args()
 
+
 def get_agency_info(agency_url):
-    agency_info = {}
-    agency_resp = ''
-    #Get agency's did and verkey:
-    try:
-        agency_req=urllib.request.urlopen('{}/agency'.format(agency_url))
-    except:
-        exc_type, exc_value, exc_traceback = sys.exc_info()
-        sys.stderr.write("Failed looking up agency did/verkey: '{}': {}\n".format(exc_type.__name__,exc_value))
-        print(json.dumps({
-            'provisioned': False,
-            'provisioned_status': "Failed: Could not retrieve agency info from: {}/agency: '{}': {}".format(agency_url,exc_type.__name__,exc_value)
-        },indent=2))
+    response = requests.get("{}/agency".format(agency_url))
+    if (not response.ok):
+        print("Unable to retrieve agency info from: {}/agency".format(agency_url))
         sys.exit(1)
-    agency_resp = agency_req.read()
-    try:
-        agency_info = json.loads(agency_resp.decode())
-    except:
-        exc_type, exc_value, exc_traceback = sys.exc_info()
-        sys.stderr.write("Failed parsing response from agency endpoint: {}/agency: '{}': {}\n".format(agency_url,exc_type.__name__,exc_value))
-        sys.stderr.write("RESPONSE: {}".format(agency_resp))
-        print(json.dumps({
-            'provisioned': False,
-            'provisioned_status': "Failed: Could not parse response from agency endpoint from: {}/agency: '{}': {}".format(agency_url,exc_type.__name__,exc_value)
-        },indent=2))
-        sys.exit(1)
-    return agency_info
+    return response.json()
 
 
-# pack once with provided key, then anoncrypt to agency
 async def send_msg(url, my_wallet, message_json, agency_verkey):
     agency_message = await crypto.pack_message(my_wallet, json.dumps(message_json), [agency_verkey], None)
     response = requests.post('{}/agency'.format(url), data=agency_message,headers={'Content-Type': 'application/octet-stream'})
@@ -157,7 +132,6 @@ async def register_agent(args):
 
     # For now, delete wallet
     await wallet.close_wallet(my_wallet)
-
 
 
 async def main():
