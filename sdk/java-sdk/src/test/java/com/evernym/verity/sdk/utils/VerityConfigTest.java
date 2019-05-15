2ab71dac5cced767718272aaf3860fff7c393a2d
Spencer Holman <spencer.holman@evernym.com>
2019-05-15 16:40:04 +0000

    update  message format

--- sdk/java-sdk/src/test/java/com/evernym/verity/sdk/utils/VerityConfigTest.java
+++ sdk/java-sdk/src/test/java/com/evernym/verity/sdk/utils/VerityConfigTest.java
@@ -9,6 +9,7 @@ import static org.junit.Assert.assertNotNull;
 
 import org.hyperledger.indy.sdk.wallet.*;
 import org.hyperledger.indy.sdk.IndyException;
+import org.hyperledger.indy.sdk.crypto.Crypto;
 import org.hyperledger.indy.sdk.did.*;
 
 import org.junit.Test;
@@ -73,6 +74,41 @@ public class VerityConfigTest {
             assertEquals(webhookUrl, verityConfig.webhookUrl);
             assertNotNull(verityConfig.getWalletHandle());
 
+            verityConfig.closeWallet();
+        } catch(Exception e) {
+            e.printStackTrace();
+            assertTrue(false);
+        } finally {
+            String walletConfig = new JSONObject().put("id", walletName).toString();
+            String walletCredentials = new JSONObject().put("key", walletKey).toString();
+            Wallet.deleteWallet(walletConfig, walletCredentials).get();
+        }
+    }
+
+    @Test
+    public void shouldGenerateCorrectUpdateWebhookMessage() throws Exception {
+        String walletName = "java_test_wallet";
+        String walletKey = "12345";
+        String webhookUrl = "http://localhost:3000";
+        try {
+            TestWallet testWallet = new TestWallet(walletName, walletKey);
+            JSONObject config = new JSONObject();
+            config.put("walletName", walletName);
+            config.put("walletKey", walletKey);
+            config.put("agencyPublicVerkey", testWallet.getAgencyPublicVerkey());
+            config.put("agencyPairwiseVerkey", testWallet.getAgencyPairwiseVerkey());
+            config.put("sdkPairwiseVerkey", testWallet.getSdkPairwiseVerkey());
+            config.put("webhookUrl", webhookUrl);
+            VerityConfig verityConfig = new VerityConfig(config.toString());
+            byte[] updateWebhookMessage = verityConfig.getUpdateWebhookMessage();
+            byte[] partiallyUnpackedMessageJWE = Crypto.unpackMessage(verityConfig.getWalletHandle(), updateWebhookMessage).get();
+            String partiallyUnpackedMessage = new JSONObject(new String(partiallyUnpackedMessageJWE)).getString("message");
+            String unpackedMessageString = MessagePackaging.unpackMessageFromAgency(verityConfig, partiallyUnpackedMessage.getBytes());
+            JSONObject unpackedMessage = new JSONObject(unpackedMessageString);
+            assertEquals("vs.service/common/0.1/update_com_method", unpackedMessage.getString("@type"));
+            assertEquals("webhook", unpackedMessage.getJSONObject("comMethod").getString("id"));
+            assertEquals("webhook", unpackedMessage.getJSONObject("comMethod").getString("type"));
+            assertEquals(webhookUrl, unpackedMessage.getJSONObject("comMethod").getString("value"));
             verityConfig.closeWallet();
         } catch(Exception e) {
             e.printStackTrace();
