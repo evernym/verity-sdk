7f1b0df191e7f98873ec1067acc326e4e17e0f85
Spencer Holman <spencer.holman@evernym.com>
2019-06-03 10:37:14 -0600

    add debug log to provision.sh

--- server/devops/dev/provision.sh
+++ server/devops/dev/provision.sh
@@ -5,3 +5,4 @@ sed -i 's/"genesis_path": "<CHANGE_ME>"/"genesis_path": "\/var\/lib\/verity-serv
 sed -i 's/"institution_name": "<CHANGE_ME>"/"institution_name": "test"/g' /etc/verity-server/vcxconfig.json
 sed -i 's/"institution_logo_url": "<CHANGE_ME>"/"institution_logo_url": "https:\/\/robohash.org\/verity-server"/g' /etc/verity-server/vcxconfig.json
 sed -i '9 a "payment_method": "null",' /etc/verity-server/vcxconfig.json
+echo "vcxconfig.json written to /etc/verity-server/vcxconfig.json"
