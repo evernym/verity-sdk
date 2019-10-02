19331438a225881a1264886b8665754a2440e1f6
Devin Fisher <devin.fisher@evernym.com>
2019-10-01 22:01:30 -0600

    fix lint checks

--- sdk/java-sdk/src/main/java/com/evernym/verity/sdk/protocols/QuestionAnswer.java
+++ sdk/java-sdk/src/main/java/com/evernym/verity/sdk/protocols/QuestionAnswer.java
@@ -4,15 +4,9 @@ import com.evernym.verity.sdk.exceptions.UndefinedContextException;
 import com.evernym.verity.sdk.exceptions.WalletException;
 import com.evernym.verity.sdk.utils.Context;
 import com.evernym.verity.sdk.utils.Util;
-import org.bouncycastle.jcajce.provider.digest.SHA3;
-import org.bouncycastle.jcajce.provider.digest.SHA3.DigestSHA3;
-import org.bouncycastle.util.encoders.Hex;
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.UUID;
 
 /**
  * Builds and sends a new encrypted agent message for the Question protocol.
@@ -38,7 +32,6 @@ public class QuestionAnswer extends Protocol {
     /**
      * Create a new Question object
      * @param forRelationship the owning pairwise DID of the relationship you want to send the question to
-     * @param notificationTitle the title of the push notification (currently only rendered in Connect.Me when questionDetail is omitted)
      * @param questionText The main text of the question (included in the message the Connect.Me user signs with their private key)
      * @param questionDetail Any additional information about the question
      * @param validResponses The possible responses. See the Verity Protocol documentation for more information on how Connect.Me will render these options.
@@ -79,15 +72,6 @@ public class QuestionAnswer extends Protocol {
         message.put("valid_responses", this.validResponses);
         this.messages.put(QuestionAnswer.ASK_QUESTION, message);
     }
-
-    private String getHashedMessage(String questionText, String responseOption) {
-        DigestSHA3 sha3256 = new SHA3.Digest256();
-        sha3256.update(questionText.getBytes(StandardCharsets.UTF_8));
-        sha3256.update(responseOption.getBytes(StandardCharsets.UTF_8));
-        sha3256.update(UUID.randomUUID().toString().getBytes(StandardCharsets.UTF_8));
-        return Hex.toHexString(sha3256.digest());
-    }
-
 
     /**
      * Sends the question message to Verity
