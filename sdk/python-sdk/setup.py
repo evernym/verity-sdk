f0fd7665ec364f339575071e8f3b1b7c04521d6f
Darko Kulic <darko.kulic@evernym.com>
2019-11-08 16:20:59 +0100

    Bumped versions

--- sdk/python-sdk/setup.py
+++ sdk/python-sdk/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="verity-sdk",
-    version="0.0.7",
+    version="0.0.8",
     author="Evernym, Inc.",
     author_email="dev@evernym.com",
     description="The official Python SDK for Evernym's Verity",
