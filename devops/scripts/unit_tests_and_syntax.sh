6b212ed343ff71997e615ee2e37348059a6cc663
Spencer Holman <spencer.holman@evernym.com>
2020-01-23 14:07:03 -0700

    separate pipeline stage for each SDK

--- devops/scripts/unit_tests_and_syntax.sh
+++ devops/scripts/unit_tests_and_syntax.sh
@@ -7,4 +7,5 @@ cd sdk/java-sdk/ && mvn test && cd - # Java SDK unit tests
 cd sdk/python-sdk/ && make lint && cd - # Python SDK linting
 cd sdk/python-sdk/ && pytest && cd - # Python SDK unit tests
 cd server && npm run lint && cd - # Mock Verity linting
-
+cd sdk/nodejs-sdk/ && npm install && npm run lint && cd - # Node.js linting
+cd sdk/nodejs-sdk/ && npm test && cd - # Node.js testing
