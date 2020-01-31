8c6b815258ef628aa23d308766724a6a944c2da3
Spencer Holman <spencer.holman@evernym.com>
2020-01-31 16:11:41 -0700

    add support for team1 environment to verity-in-docker

--- verity/scripts/entrypoint.sh
+++ verity/scripts/entrypoint.sh
@@ -1,13 +1,71 @@
 #!/bin/bash
 
-set -e
-
-# Parse Arguments
-if [ "$#" -eq 1 ]; then
-    ENTERPRISE_SEED=$1
-else
-    echo "Usage: ./entrypoint.sh ENTERPRISE_SEED"
+print_usage_and_exit() {
+    echo "usage: ./entrypoint.sh -s|--enterprise-seed ENTERPRISE_SEED -e|--environment ENVIRONMENT"
+    echo
+    echo "positional arguments:"
+    echo -e "\t -s, --enterprise-seed\t(REQUIRED) seed for Verity's primary did and verkey"
+    echo -e "\t -e, --environment\t one of \"demo\" or \"team1\". default: \"demo\""
     exit 1
+}
+
+PARAMS=""
+while (( "$#" )); do
+  case "$1" in
+    -s|--enterprise-seed)
+      ENTERPRISE_SEED=$2
+      shift 2
+      ;;
+    -e|--environment)
+      ENVIRONMENT=$2
+      shift 2
+      ;;
+    -h|--help)
+      print_usage_and_exit
+      shift 1
+      ;;
+    --) # end argument parsing
+      shift
+      break
+      ;;
+    -*|--*=) # unsupported flags
+      echo "Error: Unsupported flag $1" >&2
+      exit 1
+      ;;
+    *) # preserve positional arguments
+      PARAMS="$PARAMS $1"
+      shift
+      ;;
+  esac
+done
+
+# set positional arguments in their proper place
+eval set -- "$PARAMS"
+# Validate arguments
+if [ -z "$ENTERPRISE_SEED" ]; then
+    echo "ERROR: Missing ENTERPRISE_SEED"
+    print_usage_and_exit
+fi
+
+# Set ENVIRONMENT params
+if [ -z "$ENVIRONMENT" ]; then
+  TXN_FILE="demo.txn"
+else
+  case "$ENVIRONMENT" in
+    demo)
+      TXN_FILE="demo.txn"
+      shift
+      ;;
+    team1)
+      TXN_FILE="team1.txn"
+      shift
+      ;;
+    *)
+      echo "ERROR: Unknown ENVIRONMENT $ENVIRONMENT. Must be one of \"demo\" or \"team1\""
+      print_usage_and_exit
+      shift
+      ;;
+  esac
 fi
 
 # Start ngrok
@@ -27,6 +85,9 @@ trap trap_ctrlC SIGINT SIGTERM
 APP_CONFIG_FILE='/etc/verity/verity-application/application.conf'
 sed -i "s/NGROK_HOST/$NGROK_HOST/g" $APP_CONFIG_FILE
 
+# Set environment in configuration
+sed -i "s/TXN_FILE/$TXN_FILE/g" $APP_CONFIG_FILE
+
 # Start DynamoDB
 /root/scripts/dynamodb/start.sh
 
@@ -44,5 +105,6 @@ curl -H "Content-Type: application/json" -X POST http://127.0.0.1:9000/agency/in
 curl -X POST http://127.0.0.1:9000/agency/internal/setup/endpoint
 
 echo "Verity Bootstrapping complete. Listening on https://$NGROK_HOST"
+
 # Wait for Verity Application to exit (wait forever)
 wait
