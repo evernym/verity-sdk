006c815367a715b364b7fc01ccf4630952b65eec
Devin Fisher <devin.fisher@evernym.com>
2020-03-16 11:18:53 -0600

    allow non-provision workflow for sdk (nodejs)

--- sdk/nodejs-sdk/src/utils/Context.js
+++ sdk/nodejs-sdk/src/utils/Context.js
@@ -3,8 +3,12 @@ const URL = require('url').URL
 const indy = require('./indy')
 const utils = require('./index')
 
+const V_0_1 = "0.1"
+const V_0_2 = "0.2"
+
 module.exports = class Context {
   constructor () {
+    this.version = V_0_2
     if (arguments.length !== 0) {
       throw new Error('Invalid arguments to Context constructor. Context should be created with Context.create or Context.createWithConfig.')
     }
@@ -14,34 +18,97 @@ module.exports = class Context {
     if (typeof config === 'string' || Buffer.isBuffer(config)) {
       config = JSON.parse(config)
     }
-    Context.validateConfig(config)
+    const {version = V_0_1} = config
+
+    if (V_0_1 == version) {
+      const rtn = Context.parseV01(config)
+      await rtn.openWallet()
+      return rtn
+    }
+    else if (V_0_2 == version) {
+      const rtn = Context.parseV02(config)
+      await rtn.openWallet()
+      return rtn
+    }
+    else {
+      throw new Error(`Invalid context version -- '${version}' is not supported`)
+    }
+  }
+
+  static parseV01(config) {
+    Context.validateV01Config(config)
 
     const context = new Context()
     context.verityUrl = config.verityUrl
     context.verityPublicDID = config.verityPublicDID
-    context.verityPublicVerkey = config.verityPublicVerkey
-    context.verityPairwiseDID = config.verityPairwiseDID
-    context.verityPairwiseVerkey = config.verityPairwiseVerkey
-    context.sdkPairwiseDID = config.sdkPairwiseDID
-    context.sdkPairwiseVerkey = config.sdkPairwiseVerkey
+    context.verityPublicVerKey = config.verityPublicVerkey
+    context.domainDID = config.verityPairwiseDID
+    context.verityAgentVerKey = config.verityPairwiseVerkey
+    context.sdkVerKeyId = config.sdkPairwiseDID
+    context.sdkVerKey = config.sdkPairwiseVerkey
     context.endpointUrl = config.endpointUrl
     context.walletName = config.walletName
     context.walletPath = config.walletPath
     context.walletKey = config.walletKey
     context.buildWalletConfig(config.walletName, config.walletPath)
     context.buildWalletCredentails(config.walletKey)
-    await context.openWallet()
     return context
   }
 
-  static async create (walletName, walletKey, verityUrl, endpointUrl) {
+  static parseV02(config) {
+    Context.validateV02Config(config)
+
     const context = new Context()
+    context.verityUrl = config.verityUrl
+    context.verityPublicDID = config.verityPublicDID
+    context.verityPublicVerKey = config.verityPublicVerKey
+    context.domainDID = config.domainDID
+    context.verityAgentVerKey = config.verityAgentVerKey
+    context.sdkVerKeyId = config.sdkVerKeyId
+    context.sdkVerKey = config.sdkVerKey
+    context.endpointUrl = config.endpointUrl
+    context.walletName = config.walletName
+    context.walletPath = config.walletPath
+    context.walletKey = config.walletKey
+    context.buildWalletConfig(config.walletName, config.walletPath)
+    context.buildWalletCredentails(config.walletKey)
+    return context
+  }
+
+  
+
+  static async create(
+    walletName,
+    walletKey,
+    verityUrl,
+    domainDID = null,
+    verityAgentVerKey = null,
+    endpointUrl = null,
+    seed = null
+    ) {
+    const context = new Context()
+    context.verityUrl = verityUrl
+
+    if(domainDID) {
+      context.domainDID = domainDID
+    }
+
+    if(verityAgentVerKey) {
+      context.verityAgentVerKey = verityAgentVerKey
+    }
+
+    if(endpointUrl) {
+      context.endpointUrl = endpointUrl
+    }
+    
     context.buildWalletConfig(walletName)
     context.buildWalletCredentails(walletKey)
-    context.verityUrl = verityUrl
-    context.endpointUrl = endpointUrl
+
     await context.updateVerityInfo()
-    await context.openWallet()
+    await context.openWallet();
+
+    [context.sdkVerKeyId, context.sdkVerKey] = await indy.newDid(context, seed)
+
     return context
   }
 
@@ -65,7 +132,7 @@ module.exports = class Context {
       url.pathname = '/agency'
       const response = await utils.httpGet(url.href)
       this.verityPublicDID = response.DID
-      this.verityPublicVerkey = response.verKey
+      this.verityPublicVerKey = response.verKey
     } catch (e) {
       console.error(e)
       throw new Error(`Unable to retrieve Verity public DID and Verkey from ${this.verityUrl}`)
@@ -76,13 +143,30 @@ module.exports = class Context {
     this.walletHandle = await indy.createOrOpenWallet(this.walletConfig, this.walletCredentials)
   }
 
-  static validateConfig (config) {
+  static validateV01Config (config) {
     const requiredAttributes = [
       'verityUrl',
       'verityPublicDID',
       'verityPublicVerkey',
       'sdkPairwiseDID',
       'sdkPairwiseVerkey',
+      'walletName',
+      'walletKey'
+    ]
+    for (const attr of requiredAttributes) {
+      if (config[attr] === undefined || config[attr] === null) {
+        throw new Error('Invalid Context Configuration: missing attribute "' + attr + '"')
+      }
+    }
+  }
+
+  static validateV02Config (config) {
+    const requiredAttributes = [
+      'verityUrl',
+      'verityPublicDID',
+      'verityPublicVerKey',
+      'sdkVerKeyId',
+      'sdkVerKey',
       'walletName',
       'walletKey'
     ]
--- sdk/nodejs-sdk/test/test_Context.js
+++ sdk/nodejs-sdk/test/test_Context.js
@@ -9,14 +9,15 @@ const utils = require('../src/utils')
 const testConfig = {
   verityUrl: 'http://vas-team1.pdev.evernym.com',
   verityPublicDID: '3pjVfGmNysjiS5FiGPaa3F',
-  verityPublicVerkey: '2YEuuFaKV3gvbuCUVKGwWxXdiPtHvocV2VNJ7LK9knn1',
-  verityPairwiseDID: 'LQYTvDVJpUF76aqBoPnq2p',
-  verityPairwiseVerkey: 'BaTcmxFB6SdXh2xMhjc8thzh4P37jNbvx8QVHXeN2b5R',
-  sdkPairwiseDID: '5wEFMRrGhJUpXegY1eYxdK',
-  sdkPairwiseVerkey: '3h1FAFLNLaqyknzzE9xBrchQQC1XkLERWZuyr3ACWyy9',
+  verityPublicVerKey: '2YEuuFaKV3gvbuCUVKGwWxXdiPtHvocV2VNJ7LK9knn1',
+  domainDID: 'LQYTvDVJpUF76aqBoPnq2p',
+  verityAgentVerKey: 'BaTcmxFB6SdXh2xMhjc8thzh4P37jNbvx8QVHXeN2b5R',
+  sdkVerKeyId: '5wEFMRrGhJUpXegY1eYxdK',
+  sdkVerKey: '3h1FAFLNLaqyknzzE9xBrchQQC1XkLERWZuyr3ACWyy9',
   endpointUrl: 'http://localhost:4002',
   walletName: utils.miniId(),
-  walletKey: '12345'
+  walletKey: '12345',
+  version: '0.2'
 }
 
 function getTestConfig () {
@@ -34,11 +35,11 @@ describe('Context', () => {
     config = JSON.parse(config)
     expect(context.verityUrl).to.equal(config.verityUrl)
     expect(context.verityPublicDID).to.equal(config.verityPublicDID)
-    expect(context.verityPublicVerkey).to.equal(config.verityPublicVerkey)
-    expect(context.verityPairwiseDID).to.equal(config.verityPairwiseDID)
-    expect(context.verityPairwiseVerkey).to.equal(config.verityPairwiseVerkey)
-    expect(context.sdkPairwiseDID).to.equal(config.sdkPairwiseDID)
-    expect(context.sdkPairwiseVerkey).to.equal(config.sdkPairwiseVerkey)
+    expect(context.verityPublicVerKey).to.equal(config.verityPublicVerKey)
+    expect(context.domainDID).to.equal(config.domainDID)
+    expect(context.verityAgentVerKey).to.equal(config.verityAgentVerKey)
+    expect(context.sdkVerKeyId).to.equal(config.sdkVerKeyId)
+    expect(context.sdkVerKey).to.equal(config.sdkVerKey)
     expect(context.endpointUrl).to.equal(config.endpointUrl)
     expect(context.walletConfig).to.equal(JSON.stringify({ id: config.walletName }))
     expect(context.walletCredentials).to.equal(JSON.stringify({ key: config.walletKey }))
@@ -52,11 +53,11 @@ describe('Context', () => {
     config = JSON.parse(config)
     expect(context.verityUrl).to.equal(config.verityUrl)
     expect(context.verityPublicDID).to.equal(config.verityPublicDID)
-    expect(context.verityPublicVerkey).to.equal(config.verityPublicVerkey)
-    expect(context.verityPairwiseDID).to.equal(config.verityPairwiseDID)
-    expect(context.verityPairwiseVerkey).to.equal(config.verityPairwiseVerkey)
-    expect(context.sdkPairwiseDID).to.equal(config.sdkPairwiseDID)
-    expect(context.sdkPairwiseVerkey).to.equal(config.sdkPairwiseVerkey)
+    expect(context.verityPublicVerKey).to.equal(config.verityPublicVerKey)
+    expect(context.domainDID).to.equal(config.domainDID)
+    expect(context.verityAgentVerKey).to.equal(config.verityAgentVerKey)
+    expect(context.sdkVerKeyId).to.equal(config.sdkVerKeyId)
+    expect(context.sdkVerKey).to.equal(config.sdkVerKey)
     expect(context.endpointUrl).to.equal(config.endpointUrl)
     expect(context.walletConfig).to.equal(JSON.stringify({ id: config.walletName }))
     expect(context.walletCredentials).to.equal(JSON.stringify({ key: config.walletKey }))
@@ -68,9 +69,9 @@ describe('Context', () => {
     const requiredAttributes = [
       'verityUrl',
       'verityPublicDID',
-      'verityPublicVerkey',
-      'sdkPairwiseDID',
-      'sdkPairwiseVerkey',
+      'verityPublicVerKey',
+      'sdkVerKeyId',
+      'sdkVerKey',
       'walletName',
       'walletKey'
     ]
@@ -85,4 +86,55 @@ describe('Context', () => {
     // eslint-disable-next-line no-new
     expect(() => { new Context(getTestConfig()) }).to.throw(Error, 'Invalid arguments to Context constructor. Context should be created with Context.create or Context.createWithConfig.')
   })
+
+  it('should parse v0.1 to v0.2', async () => {
+    const v01Str = `{
+      "verityPublicVerkey": "ETLgZKeQEKxBW7gXA6FBn7nBwYhXFoogZLCCn5EeRSQV",
+      "verityPairwiseDID": "NTvSuSXzygyxWrF3scrhdc",
+      "verityUrl": "https://vas-team1.pdev.evernym.com",
+      "verityPairwiseVerkey": "ChXRWjQdrrLyksbPQZfaS3JekA4xLgD5Jg7GzXhc9zqE",
+      "walletName": "f93cb7e5-0f35-466e-ba59-11a22b8f4004",
+      "walletKey": "f8597321-c826-49a7-8edb-38b8e0fa8528",
+      "sdkPairwiseVerkey": "HZ3Ak6pj9ryFASKbA9fpwqjVh42F35UDiCLQ13J58Xoh",
+      "verityPublicDID": "Rgj7LVEonrMzcRC1rhkx76",
+      "sdkPairwiseDID": "XNRkA8tboikwHD3x1Yh7Uz"
+    }`
+
+    const ctx = await Context.createWithConfig(v01Str)
+
+    expect(ctx.domainDID).to.equal("NTvSuSXzygyxWrF3scrhdc")
+    expect(ctx.verityAgentVerKey).to.equal("ChXRWjQdrrLyksbPQZfaS3JekA4xLgD5Jg7GzXhc9zqE")
+    expect(ctx.sdkVerKey).to.equal("HZ3Ak6pj9ryFASKbA9fpwqjVh42F35UDiCLQ13J58Xoh")
+    expect(ctx.sdkVerKeyId).to.equal("XNRkA8tboikwHD3x1Yh7Uz")
+    expect(ctx.version).to.equal("0.2")
+
+  }).timeout(5000)
+
+  it('should create valid context without provisioning', async () => {
+    const ctx = await Context.create(
+      "test1",
+      "test1",
+      "https://vas-team1.pdev.evernym.com",
+      "NTvSuSXzygyxWrF3scrhdc",
+      "ChXRWjQdrrLyksbPQZfaS3JekA4xLgD5Jg7GzXhc9zqE",
+      "https://enazy4stgnrep.x.pipedream.net/",
+      "000000000000000000000000Team1VAS"
+    )
+    
+    const expected =`{
+      "verityPublicVerKey": "ETLgZKeQEKxBW7gXA6FBn7nBwYhXFoogZLCCn5EeRSQV",
+      "verityUrl": "https://vas-team1.pdev.evernym.com",
+      "verityAgentVerKey": "ChXRWjQdrrLyksbPQZfaS3JekA4xLgD5Jg7GzXhc9zqE",
+      "walletKey": "test1",
+      "sdkVerKey": "HZ3Ak6pj9ryFASKbA9fpwqjVh42F35UDiCLQ13J58Xoh",
+      "walletName": "test1",
+      "endpointUrl": "https://enazy4stgnrep.x.pipedream.net/",
+      "verityPublicDID": "Rgj7LVEonrMzcRC1rhkx76",
+      "sdkVerKeyId": "XNRkA8tboikwHD3x1Yh7Uz",
+      "version": "0.2",
+      "domainDID": "NTvSuSXzygyxWrF3scrhdc"
+    }`
+
+    expect(ctx.getConfig()).to.eql(JSON.parse(expected))
+  }).timeout(5000)
 })
