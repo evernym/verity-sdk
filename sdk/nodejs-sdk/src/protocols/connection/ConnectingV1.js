2d6275333bb894dea9454cd34d4787589eaa2d4e
Nikita Khateev <nikita.khateev@dsr-corporation.com>
2020-04-17 17:39:03 +0300

    Next steps for Node wrapper
    Signed-off-by: Nikita Khateev <nikita.khateev@dsr-corporation.com>

--- sdk/nodejs-sdk/src/protocols/connection/ConnectingV1.js
+++ sdk/nodejs-sdk/src/protocols/connection/ConnectingV1.js
@@ -1,7 +1,53 @@
 const Protocol = require('../Protocol')
+const utils = require('../utils')
 
 module.exports = class ConnectingV1 extends Protocol {
-    constructor(parentThreadId, label, base64InviteURL) {
-        
-    }
-} 
+  constructor (label, base64InviteURL, threadId = null) {
+    const msgFamily = 'connections'
+    const msgFamilyVersion = '1.0'
+    const msgQualifier = utils.constants.COMMUNITY_MSG_QUALIFIER
+    super(msgFamily, msgFamilyVersion, msgQualifier, threadId)
+
+    this.label = label
+    this.base64InviteURL = base64InviteURL
+
+    this.addMsgNames()
+  }
+
+  addMsgNames () {
+    this.msgNames.SEND_ACCEPT_INVITE = 'accept'
+    this.msgNames.GET_STATUS = 'status'
+  }
+
+  async accept (context) {
+    await this.sendMessage(context, await this.acceptMsgPacked(context))
+  }
+
+  async acceptMsgPascked (context) {
+    await this.getMessageBytes(context, this.acceptMsg())
+  }
+
+  acceptMsg () {
+    var msg = this._getBaseMessage(this.msgNames.SEND_ACCEPT_INVITE)
+    msg['~for_relationship'] = this.forRelationship
+    msg = this._addThread(msg)
+    msg.label = this.label
+    msg.invite_url = this.base64InviteURL
+    return msg
+  }
+
+  async status (context) {
+    await this.sendMessage(context, await this.statusMsgPacked(context))
+  }
+
+  async statusMsgPacked (context) {
+    return this.getMessageBytes(context, this.rejectMsg())
+  }
+
+  statusMsg () {
+    var msg = this._getBaseMessage(this.msgNames.GET_STATUS)
+    msg['~for_relationship'] = this.forRelationship
+    msg = this._addThread(msg)
+    return msg
+  }
+}
--- sdk/nodejs-sdk/src/protocols/issue-credential/IssueCredentialV1.js
+++ sdk/nodejs-sdk/src/protocols/issue-credential/IssueCredentialV1.js
@@ -18,16 +18,18 @@ module.exports = class IssueCredentialV1 extends Protocol {
     this.values = values
     this.comment = comment
     this.price = price
-    // this.created = true
+    this.created = threadId === null
 
     this.addMsgNames()
   }
 
   addMsgNames () {
-    this.msgNames.OFFER_CREDENTIAL = 'offer-credential'
-    this.msgNames.PROPOSE_CREDENTIAL = 'propose-credential'
-    this.msgNames.ISSUE_CREDENTIAL = 'issue-credential'
-    this.msgNames.REQUEST_CREDENTIAL = 'request-credential'
+    this.msgNames.OFFER_CREDENTIAL = 'offer'
+    this.msgNames.PROPOSE_CREDENTIAL = 'proposal'
+    this.msgNames.ISSUE_CREDENTIAL = 'issue'
+    this.msgNames.REQUEST_CREDENTIAL = 'request'
+    this.msgNames.REJECT_CREDENTIAL = 'reject'
+    this.msgNames.CREDENTIAL_STATUS = 'status'
   }
 
   async offerCredential (context) {
@@ -94,7 +96,40 @@ module.exports = class IssueCredentialV1 extends Protocol {
 
   issueCredentialMsg () {
     var msg = this._getBaseMessage(this.msgNames.ISSUE_CREDENTIAL)
-    // not implemented
+    msg['~for_relationship'] = this.forRelationship
+    msg.comment = this.comment
+    msg = this._addThread(msg)
+    return msg
+  }
+
+  async reject (context) {
+    await this.sendMessage(context, await this.rejectMsgPacked(context))
+  }
+
+  async rejectMsgPacked (context) {
+    return this.getMessageBytes(context, this.rejectMsg())
+  }
+
+  rejectMsg () {
+    var msg = this._getBaseMessage(this.msgNames.REJECT_CREDENTIAL)
+    msg['~for_relationship'] = this.forRelationship
+    msg.comment = this.comment
+    msg = this._addThread(msg)
+    return msg
+  }
+
+  async status (context) {
+    await this.sendMessage(context, await this.statusMsgPacked(context))
+  }
+
+  async statusMsgPacked (context) {
+    return this.getMessageBytes(context, this.rejectMsg())
+  }
+
+  statusMsg () {
+    var msg = this._getBaseMessage(this.msgNames.CREDENTIAL_STATUS)
+    msg['~for_relationship'] = this.forRelationship
+    msg = this._addThread(msg)
     return msg
   }
 }
