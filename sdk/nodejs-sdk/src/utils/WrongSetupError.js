9b938f7cc459f166309b7fa3ff8667540d8d2815
Ryan Marsh <ryan.marsh44@gmail.com>
2020-07-22 15:59:14 -0600

    fixed lint error in WrongSetupError
    Signed-off-by: Ryan Marsh <ryan.marsh44@gmail.com>

--- sdk/nodejs-sdk/src/utils/WrongSetupError.js
+++ sdk/nodejs-sdk/src/utils/WrongSetupError.js
@@ -1,3 +1,5 @@
-/**Thrown when the protocol is not properly setup for the given action */
+/**
+ *Thrown when the protocol is not properly setup for the given action
+ */
 class WrongSetupError extends Error {}
 module.exports = WrongSetupError
